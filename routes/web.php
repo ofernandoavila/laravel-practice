9702a7e36c4c93ea354a10eee24ca31d9abd9367
Fernando Avila <fernandoavilajunior@gmail.com>
2023-07-27 20:22:42 -0300

    working with routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return response('<h1>Hello World</h1>');
+});
+
+Route::get('/post/{id}', function($id) {
+    ddd($id);
+    return response('Post ' . $id );
+})->where('id', '[0-9]+');
+
+Route::get('/search', function (Request $request){
+    dd($request->name . ' @' . $request->username);
 });
